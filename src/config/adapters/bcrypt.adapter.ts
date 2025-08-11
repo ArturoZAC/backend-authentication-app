ece4d21f9e422547f0db7da47c8a0031f1cc542e
4z4c <arturoyz2105@gmail.com>
2025-08-10 21:37:46 -0500

    5CAPP

--- src/config/adapters/bcrypt.adapter.ts
+++ src/config/adapters/bcrypt.adapter.ts
@@ -2,7 +2,7 @@ import { compareSync, genSaltSync, hashSync } from "bcryptjs"
 
 export class BcryptAdapter {
 
-  public static hashed = ( password: string ) => {
+  public static hash = ( password: string ) => {
     const salt = genSaltSync();
     return hashSync(password, salt);
   }
--- src/config/adapters/jwt.adapter.ts
+++ src/config/adapters/jwt.adapter.ts
@@ -3,7 +3,7 @@ import { envs } from "../envs"
 
 export class JwtAdapter {
 
-  public static generateJWT = ( payload: any, duration: string = '1d') => {
+  public static signJWT = ( payload: any, duration: string = '1d') => {
     return new Promise((resolve, reject) => {
       jwt.sign( payload, envs.JWT_SEED, { expiresIn: duration } as SignOptions, (error, encoded) => {
         if ( error ) return reject(null)
@@ -12,7 +12,7 @@ export class JwtAdapter {
     })
   }
 
-  public static validateJWT = <T>( token: string ): Promise< T | null> => {
+  public static verifyJWT = <T>( token: string ): Promise< T | null> => {
 
     return new Promise( ( resolve, reject ) => {
       jwt.verify( token, envs.JWT_SEED , ( error, decoded ) => {
