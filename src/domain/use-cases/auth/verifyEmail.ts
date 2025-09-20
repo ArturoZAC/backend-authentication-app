e1f0899417747b0d04174453aeabe1c7f94fcb1f
4z4c <arturoyz2105@gmail.com>
2025-09-19 19:08:06 -0500

    addPropertie

--- src/domain/use-cases/auth/verifyEmail.ts
+++ src/domain/use-cases/auth/verifyEmail.ts
@@ -1,25 +1,24 @@
 import { CodeRepository } from "../../repositories/code.repository";
-import { UserRepository } from '../../repositories/user.repository';
+import { UserRepository } from "../../repositories/user.repository";
 
 export interface VerifyEmailUseCase {
-  execute ( code: string ): Promise<any>
+  execute(code: string): Promise<any>;
 }
 
 export class VerifyEmail implements VerifyEmailUseCase {
-
   public constructor(
     private readonly codeRepository: CodeRepository,
-    private readonly userRepository: UserRepository,
-  ){}
+    private readonly userRepository: UserRepository
+  ) {}
 
-  public async execute (code: string): Promise<any> {
+  public async execute(code: string): Promise<any> {
     const userId = await this.codeRepository.findByCode(code);
     await this.userRepository.changeVerify(userId);
     await this.codeRepository.deleteCode(code);
 
     return {
-      status: "Verified Email"
-    }
+      status: "Verified Email",
+      ok: true,
+    };
   }
-
 }
