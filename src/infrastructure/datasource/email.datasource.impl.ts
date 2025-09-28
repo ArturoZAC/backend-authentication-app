bbd19bafddd1534c29fa4355e5fe4b5bfb2349db
4z4c <arturoyz2105@gmail.com>
2025-09-28 15:57:18 -0500

    add test04

--- src/infrastructure/datasource/email.datasource.impl.ts
+++ src/infrastructure/datasource/email.datasource.impl.ts
@@ -5,33 +5,41 @@ import { SendEmailOptions } from "../../domain/repositories/email.repository";
 import { envs } from "../../config/envs";
 
 export class EmailDatasourceImpl implements EmailDatasource {
-  private transporter = nodemailer.createTransport({
-    // host: "smtp.gmail.com",
-    // port: 587,
-    // secure: false,
-    service: envs.MAILER_SERVICE,
-    auth: {
-      user: envs.MAILER_EMAIL,
-      pass: envs.MAILER_SECRET_KEY,
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
-  });
+  // private transporter = nodemailer.createTransport({
+  //   service: envs.MAILER_SERVICE,
+  //   auth: {
+  //     user: envs.MAILER_EMAIL,
+  //     pass: envs.MAILER_SECRET_KEY,
+  //   },
+  //   tls: {
+  //     rejectUnauthorized: false,
+  //   },
+  // });
 
   public async sendEmail(options: SendEmailOptions): Promise<boolean> {
     const { to, subject, htmlBody, attachments = [] } = options;
 
     try {
-      await this.transporter.sendMail({
-        from: "Mi app 4z4c",
-        to,
-        subject,
-        html: htmlBody,
-        attachments,
+      const response = await fetch("https://api.brevo.com/v3/smtp/email", {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          "api-key": envs.MAILER_SECRET_KEY,
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          sender: { name: "Mi app 4z4c", email: envs.MAILER_EMAIL },
+          to: [{ email: to }],
+          subject,
+          htmlContent: htmlBody,
+        }),
       });
 
-      console.log("✅ Email enviado correctamente a", to);
+      if (!response.ok) {
+        throw new Error(`Brevo error: ${response.statusText}`);
+      }
+
+      console.log(`✅ Email enviado correctamente a ${to}`);
       return true;
     } catch (error) {
       console.error("❌ Error enviando email:", error);
